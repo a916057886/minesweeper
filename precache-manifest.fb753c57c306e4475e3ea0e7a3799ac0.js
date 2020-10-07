7be870cf1e75287d4030db2ffc1b3a074ab14ac5
Cheng Chen <cheng.chen.0813@gmail.com>
2020-10-07 13:32:45 -0600

    Updates

--- precache-manifest.fb753c57c306e4475e3ea0e7a3799ac0.js
+++ precache-manifest.fb753c57c306e4475e3ea0e7a3799ac0.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8c9428b11a66150051422159f902d9be",
+    "revision": "b65c83f0916e448eb4a7451320c872dc",
     "url": "/minesweeper/index.html"
   },
   {
-    "revision": "7469ed6fc9076b45f36e",
+    "revision": "312ce7c0aa1b1b87ea6f",
     "url": "/minesweeper/static/css/main.f2f76c2e.chunk.css"
   },
   {
-    "revision": "a0ff87a6bdff72e625c1",
-    "url": "/minesweeper/static/js/2.805e7773.chunk.js"
+    "revision": "76870d0cf0a71cc6ede9",
+    "url": "/minesweeper/static/js/2.49f8f4d1.chunk.js"
   },
   {
     "revision": "90565ba0b43e1b95d2876295dfd7014d",
-    "url": "/minesweeper/static/js/2.805e7773.chunk.js.LICENSE.txt"
+    "url": "/minesweeper/static/js/2.49f8f4d1.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7469ed6fc9076b45f36e",
-    "url": "/minesweeper/static/js/main.498d8d12.chunk.js"
+    "revision": "312ce7c0aa1b1b87ea6f",
+    "url": "/minesweeper/static/js/main.50878281.chunk.js"
   },
   {
     "revision": "5d96ca1c1198200af3dc",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/minesweeper/precache-manifest.8fa928d830c5bca825eed9b6b3034b74.js"
+  "/minesweeper/precache-manifest.fb753c57c306e4475e3ea0e7a3799ac0.js"
 );
 
 self.addEventListener('message', (event) => {
